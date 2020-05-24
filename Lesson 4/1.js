79a362cbc138d7a0bd0c34c9a13c022d7ef48973
mario.base <akhmetzanovmarat@gmail.com>
2020-05-24 17:28:10 +0500

    Lesson 4. small fix

--- Lesson 4/1.js
+++ Lesson 4/1.js
@@ -3,15 +3,17 @@
 /**
  * Функция разделяет число от 0 до 999 на единицы, десятки и сотни.
  * @param {Number} num входной параметр. Число от 0 до 999
- * @returns {Object} возвращается объект
+ * @returns {Object} возвращается объект с разрядами или пустой объект, если входное значение не подходит
  */
 function NumberObject(num) {
-    if ((0 <= num <= 999) && Number.isInteger(num) && !Number.isNaN(num)) {
-        this.units = Math.floor(num % 10);
-        this.tens = Math.floor((num % 100) / 10);
+    if ((num <= 999) && (num >= 0) && Number.isInteger(num)) {
         this.hundreds = Math.floor(num / 100);
+        this.tens = Math.floor((num % 100) / 10);
+        this.units = Math.floor(num % 10);
     } else { console.log('Входным параметром должно быть целое число от 0 до 999') }
     return this;
 }
 
-console.log(new NumberObject(332));
+console.log(new NumberObject(-332));
+console.log(new NumberObject(567));
+console.log(new NumberObject(6436));
