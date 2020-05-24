bd2dc15555fe07686396e2c2bf672509126fbf5f
mario.base <akhmetzanovmarat@gmail.com>
2020-05-24 17:35:16 +0500

    Lesson 4. small fix

--- Lesson 4/1.js
+++ Lesson 4/1.js
@@ -6,11 +6,13 @@
  * @returns {Object} возвращается объект с разрядами или пустой объект, если входное значение не подходит
  */
 function NumberObject(num) {
-    if ((num <= 999) && (num >= 0) && Number.isInteger(num)) {
+    if ((num <= 999) || (num >= 0) || Number.isInteger(num)) {
+        console.log('Входным параметром должно быть целое число от 0 до 999');
+    } else {
         this.hundreds = Math.floor(num / 100);
         this.tens = Math.floor((num % 100) / 10);
         this.units = Math.floor(num % 10);
-    } else { console.log('Входным параметром должно быть целое число от 0 до 999') }
+    }
     return this;
 }
 
